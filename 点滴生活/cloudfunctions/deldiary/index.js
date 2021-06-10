d49357682d85a510fa9b7002dc1e0acc7771d7e9
Chutiiing <58101976+Chutiiing@users.noreply.github.com>
2021-06-10 17:14:10 +0800

    删除功能刷新完善 点击事件冲突解决

--- 点滴生活/cloudfunctions/deldiary/index.js
+++ 点滴生活/cloudfunctions/deldiary/index.js
@@ -6,6 +6,7 @@ cloud.init()
 // 云函数入口函数
 exports.main = async (event, context) => {
     const wxContext = cloud.getWXContext()
+    console.log("获取的id：")
     console.log(event.id)
     const db = cloud.database();
     await db.collection('diary')
@@ -14,6 +15,7 @@ exports.main = async (event, context) => {
     })
     .remove()
     .then(res=>{
+        console.log("删除的记录的数量：")
         console.log(res.stats.removed)
     })
 }
--- 点滴生活/miniprogram/pages/keepthing/noteslist.js
+++ 点滴生活/miniprogram/pages/keepthing/noteslist.js
@@ -87,21 +87,37 @@ Page({
   },
 
   delete: function(e) {
+    //确定点击的框
     var index = e.currentTarget.dataset.index;
     var index_id = this.data.noteslist[index]._id;
     console.log(index_id);
-    wx.cloud.callFunction({
-      name: 'deldiary',
-      data: {
-        id: index_id,
+    //弹框提示
+    wx.showModal({
+      title: '提示',
+      content: '确认要删除该日记?',
+      success: function (res) {
+        if (res.confirm) {
+          //提示删除成功
+          wx.showToast({
+            title: '删除成功',
+            icon: 'success',//图标
+            duration: 3000
+          })
+          console.log('用户点击确定')
+          wx.cloud.callFunction({
+            name: 'deldiary',
+            data: {
+              id: index_id,
+            }
+          }).then(res=>{
+            wx.reLaunch({
+              url: '../keepthing/noteslist',
+            })
+          })
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
       }
-    }).then(res=>{
-      wx.showToast({
-        title: '删除成功！', 
-        icon: 'success',
-        duration: 2000,
-      })
-      this.onLoad();//刷新页面
     })
   },
 
