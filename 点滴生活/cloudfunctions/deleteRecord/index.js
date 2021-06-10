dc454cb1183f4bf7d7a6efb6e01cc0e974b9b7cb
hzj2870757140 <https://github.com/hzj2870757140>
2021-06-10 21:18:58 +0800

    删除账单的时候也能更新用户额度

--- 点滴生活/cloudfunctions/deleteRecord/index.js
+++ 点滴生活/cloudfunctions/deleteRecord/index.js
@@ -3,12 +3,41 @@ const cloud = require('wx-server-sdk')
 
 cloud.init()
 
+const db = cloud.database()
+const _ = db.command
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-  var id=event.id;
-  var type=event.type;
-  const db = cloud.database()
+  var id = event.id;
+  var type = event.type;
+  var num;
+  console.log(type)
+  console.log(id)
+  if (type == "cRecord") {
+    //取消支出 所以减预算使用度
+    await db.collection(type).where(
+      {
+        _id: id,
+      }
+    ).limit(10000).get()
+      .then(res => {
+        console.log(res.data)
+        num = res.data[0].number;
+      })
+
+    var num1 = 0 - num;
+    var limit= parseFloat(num1)
+    await db.collection('user')
+  .where({
+    userid:wxContext.OPENID
+  })
+  .update({
+    data:{
+      "userlimit.1":_.inc(limit),
+    }
+  })
+
+  }
   db.collection(type).doc(id).remove()
   return true
 }
