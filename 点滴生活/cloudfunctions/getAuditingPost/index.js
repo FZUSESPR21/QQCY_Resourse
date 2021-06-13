c7c13b8a94d69179ae9558e97d5882f30da02fd0
hb594709591 <594709591@qq.com>
2021-06-13 21:05:44 +0800

    完成已审核界面，推荐和取消推荐，取消审核

---
+++ 点滴生活/cloudfunctions/getAuditingPost/index.js
@@ -0,0 +1,27 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+const db = cloud.database();
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  var data
+  await db.collection('post')
+    .where({
+      state: 1,
+    }).field({
+      _id:true,
+      userName:true,
+      createTime:true,
+      content:true,
+      userid:true,
+      recommend:true
+    })
+    .get()
+    .then(res=>{
+      data = res.data;
+    })
+    
+    return data;
+}
