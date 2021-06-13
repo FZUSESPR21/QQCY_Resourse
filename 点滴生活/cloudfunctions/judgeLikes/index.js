da7232af7410783359844da367ec2b1d71fe838e
Chutiiing <58101976+Chutiiing@users.noreply.github.com>
2021-06-13 23:01:43 +0800

    点赞点赞

---
+++ 点滴生活/cloudfunctions/judgeLikes/index.js
@@ -0,0 +1,40 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+const db = cloud.database();
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  var postdata;
+  await db.collection('post')
+  .where({
+    state:1
+  })
+  .orderBy('recommend','desc')
+  .orderBy('likes','desc')
+  .orderBy('createTime', 'desc')
+  .skip(event.length)
+  .limit(5)
+  .get()
+  .then(res=>{
+    postdata = res.data;
+  })
+  for(var i=0;i<postdata.length;i++){
+    await db.collection('like')
+    .where({
+      postid:postdata[i]._id,
+      userid:wxContext.OPENID
+    })
+    .count()
+    .then(res=>{
+      if(res.total==0){
+        isLikes = 0;
+      }
+      else{
+        isLikes = 1;
+      }
+    })
+  }
+  return isLikes; 
+}
